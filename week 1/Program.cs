7d40098fa02ca08a18662c34cd3045500dc22d62
touchApc <keith.maxey@gmail.com>
2018-08-22 21:17:49 -0500

    initial project

---
+++ week 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace week_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //this is a comment
+
+            /*This
+            is
+            a
+            comment */
+        }
+    }
+}
